d3e2fc3a73c1b11dcd500349b94972394c3c687b
Peyton Kellogg <pekellogg@hotmail.com>
2022-05-31 02:40:39 -0700

    passing specs

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,19 @@
 class PostsController < ApplicationController
 
+  before_action :find_post, only: [:show, :edit, :update, :destroy]
+
   def index
-    @posts = Post.all
+    @authors = Author.all
+    if !params[:author].blank?
+      @posts = Post.by_author(params[:author])
+    elsif !params[:date].blank?
+      params[:date] == "Today" ? @posts = Post.from_today : @posts.old_news
+    else
+      @posts = Post.all
+    end
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
@@ -15,16 +23,30 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(params)
     @post.save
-    redirect_to post_path(@post)
+    redirect_to(post_path(@post))
   end
 
   def update
-    @post = Post.find(params[:id])
     @post.update(params.require(:post))
-    redirect_to post_path(@post)
+    redirect_to(post_path(@post))
   end
 
   def edit
+  end
+
+  def destroy
+    @post.destroy
+    flash[:notice] = "Post deleted."
+    redirect_to(posts_path)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :description, :post_status, :author_id)
+  end
+
+  def find_post
     @post = Post.find(params[:id])
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,14 +4,25 @@ class Post < ActiveRecord::Base
   before_validation :make_title_case 
   belongs_to :author
 
-  #put new code here
+  def self.by_author(author_id)
+    self.where(author: author_id)
+  end
+
+  def self.from_today
+    self.where("created_at >=?", Time.zone.today.beginning_of_day)
+  end
+  
+  def self.old_news
+    self.where("created_at <?", Time.zone.today.beginning_of_day)
+  end
 
   private
 
   def is_title_case
-    if title.split.any?{|w|w[0].upcase != w[0]}
-      errors.add(:title, "Title must be in title case")
-    end
+    errors.add(:title, "Title must be in title case") if title.split.any?{|w|w[0].upcase != w[0]}
+    # if title.split.any?{|w|w[0].upcase != w[0]}
+    #   errors.add(:title, "Title must be in title case")
+    # end
   end
 
   def make_title_case
